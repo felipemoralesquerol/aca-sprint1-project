24aec4961faeaba6f20b0022a86fb9d90b070111
Felipe Morales <felipe.morales.querol@gmail.com>
2021-07-22 23:04:17 -0300

    Otros ajustes

--- src/middleware.js
+++ src/middleware.js
@@ -2,9 +2,9 @@ const { arrayInfo } = require("./info");
 
 // Funciones de middlewares
 function existeAutomotor(req, res, next) {
-    let id = req.params.id;
-    let index = arrayInfo.findIndex(auto => auto.id == id);
-    let auto = arrayInfo[index];
+    id = req.params.id;
+    index = arrayInfo.findIndex(auto => auto.id == id);
+    auto = arrayInfo[index];
     console.log(index);
     if (!auto) {
         res.status(404).send({ resultado: `Id auto ${id} no existe` });
