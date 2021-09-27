0f55f701aef1d88b456a76edc8c87e6b5573e2e3
Felipe Morales <felipe.morales.querol@gmail.com>
2021-09-26 22:42:02 -0300

    Fix en swagger

--- src/models/init.js
+++ src/models/init.js
@@ -1,65 +1,122 @@
-console.log('Información de Ambiente: ' + process.env.NODE_ENV);
+//console.log('Información de Ambiente: ' + process.env.NODE_ENV);
 
 // carga de información adicional
-let { usuarios, Usuario } = require('./usuarios');
-let { productos, Producto } = require('./productos');
-let { pedidos, pedidosEstado, Pedido } = require('./pedidos');
-let { formasDePago, FormasDePago } = require('./formaDePago');
-
+let { usuarios, Usuario } = require("./usuarios");
+let { productos, Producto } = require("./productos");
+let { pedidos, pedidosEstado, Pedido } = require("./pedidos");
+let { formasDePago, FormasDePago } = require("./formaDePago");
 
 /*  *********************** FORMAS DE PAGO************************ */
-formasDePago = [new FormasDePago('EF', "Efectivo"), new FormasDePago('TC', 'Tarjeta de Crédito'),
-new FormasDePago('TC', 'Tarjeta de Débito'), new FormasDePago('MP', 'MercadoPago')]
-console.log('Información de Formas de Pago cargada correctamente.', formasDePago);
+formasDePago = [
+  new FormasDePago("EF", "Efectivo"),
+  new FormasDePago("TC", "Tarjeta de Crédito"),
+  new FormasDePago("TC", "Tarjeta de Débito"),
+  new FormasDePago("MP", "MercadoPago"),
+];
+//console.log('Información de Formas de Pago cargada correctamente.', formasDePago);
 
 /*  ************************* USUARIOS *************************** */
-let admin = new Usuario('admin', 'admin', null, 'admin@localhost', 'admin', null, null, true);
+let admin = new Usuario(
+  "admin",
+  "admin",
+  null,
+  "admin@localhost",
+  "admin",
+  null,
+  null,
+  true
+);
 usuarios.push(admin);
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
+  let user1 = new Usuario(
+    "felipemoralesquerol",
+    "Felipe",
+    "Morales",
+    "felipe.morales.querol@gmail.com",
+    "felipe1234",
+    "221123456",
+    "Calle 506 #1617"
+  );
+  let user2 = new Usuario(
+    "juanperez",
+    "Juan",
+    "Perez",
+    "juanperez@gmail.com",
+    "juan1234"
+  );
 
-    let user1 = new Usuario('felipemoralesquerol', 'Felipe', 'Morales', 'felipe.morales.querol@gmail.com', 'felipe1234', '221123456', 'Calle 506 #1617')
-    let user2 = new Usuario('juanperez', 'Juan', 'Perez', 'juanperez@gmail.com', 'juan1234')
-
-    usuarios.push(user1)
-    usuarios.push(user2)
+  usuarios.push(user1);
+  usuarios.push(user2);
 }
-console.log('Información de Usuarios cargada correctamente.', usuarios)
-
+//console.log('Información de Usuarios cargada correctamente.', usuarios)
 
 /*  ************************* PRODUCTOS *************************** */
-if (process.env.NODE_ENV !== 'production') {
-    hamburguesa1 = new Producto('HB1', 'Hamburguesa Clasic', 'Hamburguesa clásico con JyQ', '349.99', '100');
-    hamburguesa2 = new Producto('HB2', 'Hamburguesa Verde', 'Hamburguesa en base a vegetales', '399.99', '250');
-    hamburguesa3 = new Producto('HB3', 'Hamburguesa Power', 'Hamburguesa con JyQ, tomate y cebolla', '449.99', '602');
+if (process.env.NODE_ENV !== "production") {
+  hamburguesa1 = new Producto(
+    "HB1",
+    "Hamburguesa Clasic",
+    "Hamburguesa clásico con JyQ",
+    "349.99",
+    "100"
+  );
+  hamburguesa2 = new Producto(
+    "HB2",
+    "Hamburguesa Verde",
+    "Hamburguesa en base a vegetales",
+    "399.99",
+    "250"
+  );
+  hamburguesa3 = new Producto(
+    "HB3",
+    "Hamburguesa Power",
+    "Hamburguesa con JyQ, tomate y cebolla",
+    "449.99",
+    "602"
+  );
 
-    ensaladaCesar = new Producto('EC', 'Ensalada César', 'Ensalada César', '566,99', '20');
+  ensaladaCesar = new Producto(
+    "EC",
+    "Ensalada César",
+    "Ensalada César",
+    "566,99",
+    "20"
+  );
 
-    productos = [hamburguesa1, hamburguesa2, hamburguesa3, ensaladaCesar];
+  productos = [hamburguesa1, hamburguesa2, hamburguesa3, ensaladaCesar];
 }
-console.log('Información de productos cargada correctamente.', productos)
-
+//console.log('Información de productos cargada correctamente.', productos)
 
 /*  ************************* PEDIDOS *************************** */
-if (process.env.NODE_ENV !== 'production') {
-    let pedido1 = new Pedido('felipemoralesquerol', 'EF');
-    pedido1.addProducto(hamburguesa1);
-    pedido1.addProducto(hamburguesa2);
+if (process.env.NODE_ENV !== "production") {
+  let pedido1 = new Pedido("felipemoralesquerol", "EF");
+  pedido1.addProducto(hamburguesa1);
+  pedido1.addProducto(hamburguesa2);
 
-    let pedido2 = new Pedido('juanperez', 'EF');
-    pedido2.addProducto(hamburguesa2);
+  let pedido2 = new Pedido("juanperez", "EF");
+  pedido2.addProducto(hamburguesa2);
 
-    let pedido3 = new Pedido('juanperez', 'TC');
-    pedido3.addProducto(hamburguesa1);
-    pedido3.addProducto(hamburguesa3);
-    pedido3.addProducto(ensaladaCesar);
+  let pedido3 = new Pedido("juanperez", "TC");
+  pedido3.addProducto(hamburguesa1);
+  pedido3.addProducto(hamburguesa3);
+  pedido3.addProducto(ensaladaCesar);
 
-    // carga de algunos pedidos iniciales
-    addPedido(pedido1);
-    addPedido(pedido2);
-    addPedido(pedido3);
+  // carga de algunos pedidos iniciales
+  addPedido(pedido1);
+  addPedido(pedido2);
+  addPedido(pedido3);
 }
 
-console.log('Información de pedidos cargada correctamente.', pedidos)
+//console.log('Información de pedidos cargada correctamente.', pedidos)
 
-module.exports = { usuarios, Usuario, productos, Producto, pedidos, pedidosEstado, Pedido, formasDePago, FormasDePago }
+module.exports = {
+  usuarios,
+  Usuario,
+  productos,
+  Producto,
+  pedidos,
+  pedidosEstado,
+  Pedido,
+  formasDePago,
+  FormasDePago,
+};
--- src/routes/program.js
+++ src/routes/program.js
@@ -1,19 +1,21 @@
-const express = require('express');
+const express = require("express");
 let router = express.Router();
 
 /**
  * @swagger
- * /programa:
+ * /:
  *  get:
+ *    description: Referencia al programa especial
  *    tags: [generales]
  *    summary: programa
- *    description : Resto
  *    responses:
- *     200: 
- *       description: Descripción de la versión
+ *      200:
+ *        description: Descripción de la versión
+ *      304:
+ *        description: Descripción de la versión
  */
-router.get('/', function (req, res) {
-    res.send({ programa: "Resto v1.0.2" })
+router.get("/", function (req, res) {
+  res.send({ programa: "Resto v2.0.0" });
 });
 
 module.exports = router;
